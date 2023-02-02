9d50e614e20ad1e51e77b93ebb2e0e40c61947e5
Betty Demissie <116000530+bettydemissie@users.noreply.github.com>
2023-02-02 19:43:44 +0000

    Add files via upload

---
+++ BinarySearch.zip/week2data/BinarySearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace DataStructuresBinarySearch
+{
+	public class BinarySearch
+	{
+		private int[] arrayvalue;
+		private int valuetofind;
+
+		public BinarySearch(int[] arrayvalue, int valuetofind)
+		{
+			this.arrayvalue = arrayvalue;
+			this.valuetofind = valuetofind;
+        }
+		
+
+		public static int Binarysearch(int[] arrayvalue, int valuetofind)
+		{
+            Array.Sort(arrayvalue);
+
+            int start = 0;
+			int end = arrayvalue.Length - 1;
+
+			while(start <= end)
+			{
+				int mid = (start + end) / 2;
+
+				if (arrayvalue[mid] == valuetofind)
+				{
+					return(mid);
+				}
+				else if (arrayvalue[mid] > valuetofind)
+				{
+					start = mid - 1;
+				}
+				else if (arrayvalue[mid] < valuetofind)
+				{
+                    start = mid + 1;
+                }
+				else
+				{
+					end = mid - 1;
+				}
+			}
+			Console.WriteLine("DIDNT FIND ANYTHING");
+			return -1;
+
+		}
+	}
+}
+
---
+++ BinarySearch.zip/week2data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructuresBinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 13, 32, 45, 56, 67, 1, 2, 5};
+
+            /*foreach (int i in array)
+            {
+     
+                Console.WriteLine(i);
+            }*/
+
+            int findvalueindex = BinarySearch.Binarysearch(array, 56);
+            Console.WriteLine($"Find value index in array: " + findvalueindex);
+           
+           
+        }
+    }
+}
+
